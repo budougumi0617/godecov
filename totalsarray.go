09fcefd26e20487a68b01198b795d943613a4d2c
budougumi0617 <budougumi0617@gmail.com>
2018-11-22 20:10:35 +0900

    :green_heart: Add test

---
+++ totalsarray.go
@@ -0,0 +1,83 @@
+// Copyright (c) 2018.  Yoichiro Shimizu @budougumi0617
+
+package godecov
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+// TotalsArray is included many Codcov responses.
+type TotalsArray struct {
+	// C    int           `json:"C"`
+	Files         int    `json:"f"` // [0] files count
+	Lines         int    `json:"n"` // [1] lines count
+	Hits          int    `json:"h"` // [2] hits count
+	Partials      int    `json:"p"` // [3] partials count
+	Missed        int    `json:"m"` // [4] missed count
+	CoverageRatio string `json:"c"` // [5] coverage ratio
+	Sessions      int    `json:"s"` // [9] sessions count
+	Messages      int    `json:"M"` // messages count
+	N             int    `json:"N"` // Unknown count
+	Branches      int    `json:"b"` // branches count
+	Methods       int    `json:"d"` // methods count
+	// Diff []interface{} `json:"diff"`
+}
+
+// UnmarshalJSON creates TotalsArray from []interface{}.
+func (ta *TotalsArray) UnmarshalJSON(data []byte) error {
+	var row interface{}
+	err := json.Unmarshal(data, &row)
+	if err != nil {
+		return err
+	}
+	arr, ok := row.([]interface{})
+	if !ok {
+		return errors.New("failed type cast")
+	}
+	// TODO Use NullInt
+	f, ok := arr[0].(float64)
+	if !ok {
+		return errors.New("failed type cast f")
+	}
+	ta.Files = int(f)
+	n, ok := arr[1].(float64)
+	if !ok {
+		return errors.New("failed type cast n")
+	}
+	ta.Lines = int(n)
+	h, ok := arr[2].(float64)
+	if !ok {
+		return errors.New("failed type cast h")
+	}
+	ta.Hits = int(h)
+	p, ok := arr[3].(float64)
+	if !ok {
+		return errors.New("failed type cast p")
+	}
+	ta.Partials = int(p)
+	m, ok := arr[4].(float64)
+	if !ok {
+		return errors.New("failed type cast m")
+	}
+	ta.Missed = int(m)
+	c, ok := arr[5].(string)
+	if !ok {
+		return errors.New("failed type cast c")
+	}
+	ta.CoverageRatio = c
+
+	// 6-9, Unknown values
+
+	s, ok := arr[9].(float64)
+	if !ok {
+		return errors.New("failed type cast s")
+	}
+	ta.Sessions = int(s)
+	return nil
+}
+
+// TODO Define MarshalJSON for TotalsArray
+// func (ta *TotalsArray) MarshalJSON()([]byte, error){
+//
+// }
---
+++ totalsarray_test.go
@@ -0,0 +1,57 @@
+package godecov
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestTotalsArray_UnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		subject   string
+		in        string
+		want      TotalsArray
+		wantError bool
+		err       error
+	}{
+		{
+			subject: "simple",
+			in:      "{\"totals\":[6,22,18,4,0,\"81.81818\",0,0,0,1,0,0,null]}",
+			want: TotalsArray{
+				Files:         6,
+				Lines:         22,
+				Hits:          18,
+				Partials:      4,
+				Missed:        0,
+				CoverageRatio: "81.81818",
+				Sessions:      1,
+				Messages:      0,
+				N:             0,
+				Branches:      0,
+				Methods:       0,
+			},
+			wantError: false,
+			// err: fmt.Errorf(""),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.subject, func(t *testing.T) {
+
+			var got struct {
+				Totals TotalsArray `json:"totals"`
+			}
+			err := json.Unmarshal([]byte(tt.in), &got)
+
+			if !tt.wantError && err != nil {
+				t.Fatalf("want no err, but has error %#v", err)
+			}
+			if tt.wantError && !reflect.DeepEqual(err, tt.err) {
+				t.Fatalf("want %#v, but %#v", tt.err, err)
+			}
+
+			if got.Totals != tt.want {
+				t.Errorf("want %#v, but got %#v", tt.want, got.Totals)
+			}
+		})
+	}
+}
