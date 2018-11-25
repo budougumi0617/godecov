608dff098332accccf0b99d56e61bf7590d1299a
budougumi0617 <budougumi0617@gmail.com>
2018-11-25 22:10:00 +0900

    :green_heart: Add test for MarshalJSON

--- totalsarray_test.go
+++ totalsarray_test.go
@@ -55,3 +55,50 @@ func TestTotalsArray_UnmarshalJSON(t *testing.T) {
 		})
 	}
 }
+
+func TestTotalsArray_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		subject   string
+		in        TotalsArray
+		want      string
+		wantError bool
+		err       error
+	}{
+		{
+			subject: "simple",
+			in: TotalsArray{
+				Files:         6,
+				Lines:         22,
+				Hits:          18,
+				Partials:      4,
+				Missed:        0,
+				CoverageRatio: "81.81818",
+				Sessions:      1,
+				Messages:      0,
+				N:             0,
+				Branches:      0,
+				Methods:       0,
+			},
+			want:      "{\"totals\":[6,22,18,4,0,\"81.81818\",0,0,0,1,0,0,null]}",
+			wantError: false,
+			// err: fmt.Errorf(""),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.subject, func(t *testing.T) {
+
+			got, err := json.Marshal(tt.in)
+
+			if !tt.wantError && err != nil {
+				t.Fatalf("want no err, but has error %#v", err)
+			}
+			if tt.wantError && !reflect.DeepEqual(err, tt.err) {
+				t.Fatalf("want %#v, but %#v", tt.err, err)
+			}
+
+			if string(got) != tt.want {
+				t.Errorf("want \n%s\nbut got \n%s", tt.want, string(got))
+			}
+		})
+	}
+}
