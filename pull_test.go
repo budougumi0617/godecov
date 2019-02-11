ba7db34da1b24b9acdf0856ac4611d09b670725f
budougumi0617 <budougumi0617@gmail.com>
2019-02-11 21:30:01 +0900

    :+1: Add test

---
+++ pull_test.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2019. Yoichiro Shimizu @budougumi0617
+
+package godecov_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/budougumi0617/godecov"
+)
+
+func TestClient_GetPull(t *testing.T) {
+	// Confirm degradation
+	user := "budougumi0617"
+	repo := "godecov"
+	no := 10
+	tok := os.Getenv("CODECOV_API_TOKEN")
+	cli := godecov.NewClient(tok)
+	_, err := cli.GetPull(user, repo, no)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
