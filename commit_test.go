8803b7b4686e608bc25709e027398920c39ef51e
budougumi0617 <budougumi0617@gmail.com>
2019-02-12 09:11:04 +0900

    :+1: Add tests

--- commit_test.go
+++ commit_test.go
@@ -21,3 +21,42 @@ func TestClient_GetCommit(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestClient_GetCommitEmptyUser(t *testing.T) {
+	// Confirm degradation
+	user := ""
+	repo := "gopl"
+	sha := "d4aa993"
+	tok := os.Getenv("CODECOV_API_TOKEN")
+	cli := godecov.NewClient(tok)
+	_, err := cli.GetCommit(user, repo, sha)
+	if err == nil {
+		t.Fatal("expect error")
+	}
+}
+
+func TestClient_GetCommitEmptyRepo(t *testing.T) {
+	// Confirm degradation
+	user := "budougumi0617"
+	repo := ""
+	sha := "d4aa993"
+	tok := os.Getenv("CODECOV_API_TOKEN")
+	cli := godecov.NewClient(tok)
+	_, err := cli.GetCommit(user, repo, sha)
+	if err == nil {
+		t.Fatal("expect error")
+	}
+}
+
+func TestClient_GetCommitEmptySha(t *testing.T) {
+	// Confirm degradation
+	user := "budougumi0617"
+	repo := "gopl"
+	sha := ""
+	tok := os.Getenv("CODECOV_API_TOKEN")
+	cli := godecov.NewClient(tok)
+	_, err := cli.GetCommit(user, repo, sha)
+	if err == nil {
+		t.Fatal("expect error")
+	}
+}
